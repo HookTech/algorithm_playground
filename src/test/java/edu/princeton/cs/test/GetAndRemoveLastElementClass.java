b8cb7352d1ed5ec6be085782e766a0b147d0a43c
PhiloHe <HookTech9@gmail.com>
2018-03-29 08:07:10 +0800

    init getandremovelastelementclass

---
+++ src/test/java/edu/princeton/cs/test/GetAndRemoveLastElementClass.java
@@ -0,0 +1,16 @@
+package edu.princeton.cs.test;
+
+import org.testng.annotations.Test;
+
+public class GetAndRemoveLastElementClass implements StackAndQueueCallMethod {
+    /*** self practice function **/
+    /***standard resolve***/
+    @Override
+    public void callStackAndQueueAlgoMethod() {
+        getAndRemoveLastElementTest();
+    }
+
+    @Test(groups = "StackAndQueue")
+    public void getAndRemoveLastElementTest(){}
+
+}
