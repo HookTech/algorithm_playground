abb58684eccc86e2e9a0e72143c37c58e23d1251
Philo He <philo.he@sap.com>
2019-01-08 17:50:59 +0800

    half

--- src/test/java/ps/philo/playground/leetcode/medium/P5_LongestPalindromicSubstring.java
+++ src/test/java/ps/philo/playground/leetcode/medium/P5_LongestPalindromicSubstring.java
@@ -20,47 +20,55 @@ import org.testng.annotations.Test;
  * Output: "bb"
  */
 public class P5_LongestPalindromicSubstring {
-	@Test
-	public void test() {
-		Assert.assertEquals(longestPalindrome("babad"), "aba");
-		Assert.assertEquals(longestPalindrome("cbbd"), "bb");
-		Assert.assertEquals(longestPalindrome("abcda"), "b");
-		Assert.assertEquals(longestPalindrome("abcaa"), "aa");
-	}
+  @Test
+  public void test() {
+    Assert.assertEquals(longestPalindrome("babad"), "aba");
+    Assert.assertEquals(longestPalindrome("cbbd"), "bb");
+    Assert.assertEquals(longestPalindrome("abcda"), "b");
+    Assert.assertEquals(longestPalindrome("abcaa"), "aa");
+  }
 
-	public String longestPalindrome(String s) {
-		if (s == null || s.isEmpty()) return "";
-		int l = s.length();
-		String[][] subs = new String[l + 1][l];
-//		String[] subs = new String[l+1];
-		for (int k = 0; k < l; k++) {
-			subs[k][k] = s.substring(k, k + 1);
-			subs[k + 1][k] = "";
-		}
-		for (int g = 1; g < l; g++) {
-			for (int i = 0; i < l - g; i++) {
-				int j = i + g;
-				if (subs[i + 1][j - 1].length() != j - i - 1) {
-					String left = subs[i][j - 1];
-					String right = subs[i + 1][j];
-					subs[i][j] = left.length() >= right.length() ? left : right;
-				} else {
-					if (s.charAt(i) == s.charAt(j)) {
-						subs[i][j] = s.substring(i, j + 1);
-					} else {
-						if (subs[i + 1][j - 1].equals(s.substring(i, i + 1))) {
-							subs[i][j] = subs[i][j - 1];
-						} else if (subs[i + 1][j - 1].equals(s.substring(j, j + 1))) {
-							subs[i][j] = subs[i + 1][j];
-						} else if (subs[i + 1][j - 1].equals("")) {
-							subs[i][j] = s.substring(i, i + 1);
-						} else {
-							subs[i][j] = subs[i + 1][j - 1];
-						}
-					}
-				}
-			}
-		}
-		return subs[0][l - 1];
-	}
+  public String longestPalindrome(String s) {
+    if (s == null || s.isEmpty()) return "";
+    int l = s.length();
+//		String[][] subs = new String[l + 1][l];
+    String[] subs = new String[l + 1];
+    for (int k = 0; k < l; k++) {
+      subs[project(k, k, 0, l)] = s.substring(k, k + 1);
+      subs[project(k + 1, k, 0, l)] = "";
+    }
+    for (int g = 1; g < l; g++) {
+      for (int i = 0; i < l - g; i++) {
+        int j = i + g;
+        if (subs[project(i+1,j-1,g,l)].length() != j - i - 1) {
+          String left = subs[i][j - 1];
+          String right = subs[i + 1][j];
+          subs[i][j] = left.length() >= right.length() ? left : right;
+        } else {
+          if (s.charAt(i) == s.charAt(j)) {
+            subs[i][j] = s.substring(i, j + 1);
+          } else {
+            if (subs[i + 1][j - 1].equals(s.substring(i, i + 1))) {
+              subs[i][j] = subs[i][j - 1];
+            } else if (subs[i + 1][j - 1].equals(s.substring(j, j + 1))) {
+              subs[i][j] = subs[i + 1][j];
+            } else if (subs[i + 1][j - 1].equals("")) {
+              subs[i][j] = s.substring(i, i + 1);
+            } else {
+              subs[i][j] = subs[i + 1][j - 1];
+            }
+          }
+        }
+      }
+    }
+    return subs[0][l - 1];
+  }
+
+  private int project(int i, int j, int gap, int length) {
+    if (i > j) {
+      return length - 1;
+    } else {
+      return i + gap;
+    }
+  }
 }
