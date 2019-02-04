ff2dd2519547e01d23cd156b0fe614e087ec1259
HookTech <hhj-max@163.com>
2019-02-04 17:48:21 +0800

    NQueen

---
+++ src/test/java/ps/philo/playground/BackTrack/NQueenProblem.java
@@ -0,0 +1,38 @@
+package ps.philo.playground.BackTrack;
+
+/**
+ * philo
+ * # 2/1/19
+ */
+public class NQueenProblem {
+	public int num(int n) {
+		if (n < 1) {
+			return 0;
+		}
+		int[] record = new int[n];
+		return process(0, record, n);
+	}
+
+	public int process(int i, int[] record, int n) {
+		if (i == n) {
+			return 1;
+		}
+		int res = 0;
+		for (int j = 0; j < n; j++) {
+			if (isValid(record, i, j)) {
+				record[i] = j;
+				res += process(i + 1, record, n);
+			}
+		}
+		return res;
+	}
+
+	public boolean isValid(int[] record, int i, int j) {
+		for (int k = 0; k < i; k++) {
+			if (j == record[k] || Math.abs(record[k] - j) == Math.abs(i - k)) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
