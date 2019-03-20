f8ae188a64b42c44555e5e9f87c40cfaeabb9be5
HookTech <hhj-max@163.com>
2019-03-20 08:30:36 +0800

    swap

---
+++ src/test/java/ps/philo/playground/leetcode/medium/P24_SwapNodesInPairs.java
@@ -0,0 +1,48 @@
+package ps.philo.playground.leetcode.medium;
+
+import edu.princeton.cs.test.TestUtil;
+import org.testng.annotations.Test;
+
+import static ps.philo.playground.CommonStructure.ListNode;
+/**
+ * philo
+ * Given a linked list, swap every two adjacent nodes and return its head.
+ *
+ * You may not modify the values in the list's nodes, only nodes itself may be changed.
+ *
+ *
+ *
+ * Example:
+ *
+ * Given 1->2->3->4, you should return the list as 2->1->4->3.
+ * # 3/20/19
+ */
+public class P24_SwapNodesInPairs {
+	@Test
+	public void test() throws Exception {
+		ListNode<Integer> head = TestUtil.generateListFromSeries(1,2,3,4);
+		TestUtil.assertList(
+			swapPairs(head),
+			2,1,4,3
+		);
+
+	}
+	public ListNode swapPairs(ListNode head) {
+		ListNode nHead = swap(head);
+		head = nHead;
+		while(head != null && head.next != null){
+			head.next.next = swap(head.next.next);
+			head = head.next.next;
+		}
+		return nHead;
+	}
+
+	ListNode swap(ListNode p){
+		if(p == null || p.next == null) return p;
+		ListNode gradeChild = p.next.next;
+		ListNode child = p.next;
+		p.next = gradeChild;
+		child.next = p;
+		return child;
+	}
+}
