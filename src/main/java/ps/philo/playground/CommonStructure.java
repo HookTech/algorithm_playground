8b6e7da0a997b438c79d1ec53fb0cdc4ea8b7adb
HookTech <hhj-max@163.com>
2019-03-24 12:22:42 +0800

    non null tree test utils class

--- src/main/java/ps/philo/playground/CommonStructure.java
+++ src/main/java/ps/philo/playground/CommonStructure.java
@@ -1,6 +1,8 @@
 package ps.philo.playground;
 
 import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 /**
@@ -10,106 +12,144 @@ import java.util.Queue;
  * @create 2018-03-27 9:31 AM
  **/
 public class CommonStructure {
-    public static class ListNode<T>{
-        public T value;
-        public ListNode<T> next;
-        public ListNode(T data){
-            value = data;
-        }
-    }
+	public static class ListNode<T> {
+		public T value;
+		public ListNode<T> next;
 
-    public static class TreeNode<T>{
-        public T value;
-        public TreeNode<T> left;
-        public TreeNode<T> right;
-        public int depth = 0;
-        public TreeNode(T v, TreeNode l, TreeNode r){
-            this.value = v;
-            this.left = l;
-            this.right = r;
-        }
+		public ListNode(T data) {
+			value = data;
+		}
+	}
 
-        public TreeNode(T v, TreeNode l, TreeNode r, int d){
-            this(v, l, r);
-            this.depth = d;
-        }
+	public static class TreeNode<T> {
+		public T value;
+		public TreeNode<T> left;
+		public TreeNode<T> right;
+		public int depth = 0;
 
-        private String preOrderTraversal(TreeNode<T> root){
-            StringBuilder builder = new StringBuilder();
-            if(root != null){
-                builder.append(root.value);
-                builder.append(preOrderTraversal(root.left));
-                builder.append(preOrderTraversal(root.right));
-            }
-            return builder.toString();
-        }
+		public TreeNode(T v, TreeNode l, TreeNode r) {
+			this.value = v;
+			this.left = l;
+			this.right = r;
+		}
 
-        private String deepFirst(){
-            StringBuilder builder = new StringBuilder();
-            if(left == null && right == null){
-                builder.append(value);
-            }
-            else if(left != null && right == null) {
-                builder.append("  ").append(value).append("  ")
-                        .append("\n")
-                        .append(" ").append("/").append(" ").append(" ").append(" ")
-                        .append("\n")
-                        .append(left.toString()).append("   ").append(" ");
-            }
-            else if(left == null && right != null){
-                builder.append("  ").append(value).append("  ")
-                        .append("\n")
-                        .append(" ").append(" ").append(" ").append("\\").append(" ")
-                        .append("\n")
-                        .append(" ").append("   ").append(right.toString());
-            }
-            else {
-                builder.append("  ").append(value).append("  ")
-                        .append("\n")
-                        .append(" ").append("/").append(" ").append("\\").append(" ")
-                        .append("\n")
-                        .append(left.toString()).append("   ").append(right.toString());
-            }
-            return builder.toString();
-        }
-    }
+		public TreeNode(T v, TreeNode l, TreeNode r, int d) {
+			this(v, l, r);
+			this.depth = d;
+		}
 
-    public static class Tree<T>{
-        TreeNode<T> root;
-        int maxDepth;
+		private String preOrderTraversal(TreeNode<T> root) {
+			StringBuilder builder = new StringBuilder();
+			if (root != null) {
+				builder.append(root.value);
+				builder.append(preOrderTraversal(root.left));
+				builder.append(preOrderTraversal(root.right));
+			}
+			return builder.toString();
+		}
 
-        public Tree(TreeNode root, int maxDepth){
-            this.root = root;
-            this.maxDepth = maxDepth;
-        }
+		private String deepFirst() {
+			StringBuilder builder = new StringBuilder();
+			if (left == null && right == null) {
+				builder.append(value);
+			} else if (left != null && right == null) {
+				builder.append("  ").append(value).append("  ")
+					.append("\n")
+					.append(" ").append("/").append(" ").append(" ").append(" ")
+					.append("\n")
+					.append(left.toString()).append("   ").append(" ");
+			} else if (left == null && right != null) {
+				builder.append("  ").append(value).append("  ")
+					.append("\n")
+					.append(" ").append(" ").append(" ").append("\\").append(" ")
+					.append("\n")
+					.append(" ").append("   ").append(right.toString());
+			} else {
+				builder.append("  ").append(value).append("  ")
+					.append("\n")
+					.append(" ").append("/").append(" ").append("\\").append(" ")
+					.append("\n")
+					.append(left.toString()).append("   ").append(right.toString());
+			}
+			return builder.toString();
+		}
+	}
 
-        @Override
-        public String toString() {
-            Queue<TreeNode<T>> nodes = new ArrayDeque<>();
-            StringBuilder builder = new StringBuilder();
-            nodes.add(root);
-            while (!nodes.isEmpty()){
-                TreeNode<T> curNode = nodes.poll();
-                builder.append("  ").append(curNode.value).append("  -- ").append(curNode.depth)
-                        .append("\n");
-                addToQueue(nodes,curNode.left);
-                addToQueue(nodes,curNode.right);
-            }
-            return builder.toString();
-        }
+	public static class Tree<T> {
+		TreeNode<T> root;
+		int maxDepth;
 
-        private void printWhiteSpace(StringBuilder builder, int count){
-            for (int i = 0; i < count; i++) {
-                builder.append(" ");
-            }
-        }
+		public Tree(TreeNode root, int maxDepth) {
+			this.root = root;
+			this.maxDepth = maxDepth;
+		}
 
-        private <T> void addToQueue(Queue<TreeNode<T>> queue, TreeNode<T> item){
-            if(item != null){
-                queue.add(item);
-            }
-        }
-    }
+		@Override
+		public String toString() {
+			Queue<TreeNode<T>> nodes = new ArrayDeque<>();
+			StringBuilder builder = new StringBuilder();
+			nodes.add(root);
+			while (!nodes.isEmpty()) {
+				TreeNode<T> curNode = nodes.poll();
+				builder.append("  ").append(curNode.value).append("  -- ").append(curNode.depth)
+					.append("\n");
+				addToQueue(nodes, curNode.left);
+				addToQueue(nodes, curNode.right);
+			}
+			return builder.toString();
+		}
 
+		private void printWhiteSpace(StringBuilder builder, int count) {
+			for (int i = 0; i < count; i++) {
+				builder.append(" ");
+			}
+		}
 
+		private <T> void addToQueue(Queue<TreeNode<T>> queue, TreeNode<T> item) {
+			if (item != null) {
+				queue.add(item);
+			}
+		}
+	}
+
+	public static Object[] tree2Array(TreeNode<Object> root) {
+		List<Object> res = new LinkedList<>();
+		ArrayDeque<TreeNode<Object>> bfs = new ArrayDeque<>();
+		bfs.push(root);
+		fetchNode(bfs, res);
+		return res.toArray();
+	}
+
+	private static void fetchNode(ArrayDeque<TreeNode<Object>> q, List<Object> collector) {
+		if (!q.isEmpty()) {
+			TreeNode<Object> node = q.poll();
+			collector.add(node.value);
+			if (node.left != null)
+				q.push(node.left);
+			if (node.right != null)
+				q.push(node.right);
+			fetchNode(q, collector);
+		}
+	}
+
+	public static TreeNode<Object> array2Tree(Object[] items) {
+		int real = 1, last = items.length;
+		TreeNode<Object>[] index = new TreeNode[items.length];
+		TreeNode<Object> root = new TreeNode<>(items[tran(real)], null, null);
+		index[tran(real)] = root;
+		while (++real <= last) {
+			TreeNode<Object> nNode = new TreeNode<>(items[tran(real)], null, null);
+			if (real % 2 == 0) {
+				index[tran(real / 2)].left = nNode;
+			} else {
+				index[tran(real / 2)].right = nNode;
+			}
+			index[tran(real)] = nNode;
+		}
+		return root;
+	}
+
+	private static int tran(int realIndex) {
+		return realIndex - 1;
+	}
 }
---
+++ src/test/java/ps/philo/playground/leetcode/easy/P110_BalancedBinaryTree.java
@@ -0,0 +1,60 @@
+package ps.philo.playground.leetcode.easy;
+
+import edu.princeton.cs.test.TestUtil;
+import org.testng.annotations.Test;
+
+import static ps.philo.playground.CommonStructure.*;
+
+/**
+ * philo
+ * Given a binary tree, determine if it is height-balanced.
+ * <p>
+ * For this problem, a height-balanced binary tree is defined as:
+ * <p>
+ * a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+ * <p>
+ * Example 1:
+ * <p>
+ * Given the following tree [3,9,20,null,null,15,7]:
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * Return true.
+ * <p>
+ * Example 2:
+ * <p>
+ * Given the following tree [1,2,2,3,3,null,null,4,4]:
+ * <p>
+ * 1
+ * / \
+ * 2   2
+ * / \
+ * 3   3
+ * / \
+ * 4   4
+ * Return false.
+ * # 3/24/19
+ */
+public class P110_BalancedBinaryTree {
+
+	@Test
+	public void test() {
+		Object[] items = new Integer[]{1, 2, 2, 3};
+		TestUtil.assertEqualAndPrintToInfo(tree2Array(array2Tree(items)), items);
+	}
+
+	public boolean isBalanced(TreeNode root) {
+		if (root == null) return true;
+		int sub = deep(root.left) - deep(root.right);
+		if (sub > 1 || sub < -1) return false;
+		return isBalanced(root.left) && isBalanced(root.right);
+	}
+
+	public int deep(TreeNode node) {
+		if (node == null) return -1;
+		return Math.max(deep(node.left), deep(node.right)) + 1;
+	}
+}
